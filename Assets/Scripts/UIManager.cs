fda43574964fbefa356da6dfb8b433bdb2098f84
nmxi <mikki1118_2@hotmail.co.jp>
2020-04-23 00:12:42 +0900

    v1.0.0

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject licenceViewObj;
+
+    private bool isShow;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            CloseLicenceView();
+    }
+
+    public void ShowLicenceView()
+    {
+        if (isShow)
+            return;
+
+        licenceViewObj.SetActive(true);
+        isShow = true;
+    }
+
+    public void CloseLicenceView()
+    {
+        if (!isShow)
+            return;
+
+        licenceViewObj.SetActive(false);
+        isShow = false;
+    }
+}
